fc0d762a931e7dbeebe2ab19b003b8754a2d66a7
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-18 15:55:03 +0200

    exe 1 part 2

--- exercice 1/assets/js/script.js
+++ exercice 1/assets/js/script.js
@@ -1,23 +1,16 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-context.fillStyle = '#00A550';
-context.fillRect(100,50,100,25);
-context.fillRect(75,75,150,25);
-
+context.beginPath();
+context.moveTo(50, 100);
+context.lineTo(90,100);
+context.lineTo(70,150);
+context.closePath();
+context.fillStyle = '#AA6522';
+context.fill();
 
 context.beginPath();
-context.fillStyle = '#CECECE';
-context.lineWidth="1";
-context.arc(87, 120, 20, 0, 2 * Math.PI);
+context.moveTo(50, 100);
+context.quadraticCurveTo(75, 50, 90,100);
+context.fillStyle ='#8A0908';
 context.fill();
-context.stroke();
-context.closePath();
-
-context.beginPath();
-context.fillStyle = '#CECECE';
-context.lineWidth="1";
-context.arc(212, 120, 20, 0, 2 * Math.PI);
-context.fill();
-context.stroke();
-context.closePath();
