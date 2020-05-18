a77b1fbffc0cc030df68092c1ddf6b78ef75fd69
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-18 16:15:04 +0200

    correction

--- exercice 1/assets/js/script.js
+++ exercice 1/assets/js/script.js
@@ -5,10 +5,9 @@ context.fillStyle = '#00A550';
 context.fillRect(100,50,100,25);
 context.fillRect(75,75,150,25);
 
-
 context.beginPath();
 context.fillStyle = '#CECECE';
-context.lineWidth="1";
+context.lineWidth ='1';
 context.arc(87, 120, 20, 0, 2 * Math.PI);
 context.fill();
 context.stroke();
@@ -16,7 +15,7 @@ context.closePath();
 
 context.beginPath();
 context.fillStyle = '#CECECE';
-context.lineWidth="1";
+context.lineWidth ='1';
 context.arc(212, 120, 20, 0, 2 * Math.PI);
 context.fill();
 context.stroke();
