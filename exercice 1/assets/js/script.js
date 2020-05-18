b390eb7be653cc335a46ba2f1540d9bcd7f94c6e
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-18 15:19:39 +0200

    exercice 3

--- exercice 1/assets/js/script.js
+++ exercice 1/assets/js/script.js
@@ -1,28 +1,23 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
-// Murs maison
-context.fillStyle = 'lightblue';
-context.fillRect(50,150,100,100);
-// Porte
-context.fillStyle = '#CACACA';
-context.fillRect(85,210,30,40);
-// fenetre gauche
-context.fillStyle = '#CACACA';
-context.fillRect(60,160,20,20);
-// fenetre droite
-context.fillStyle = '#CACACA';
-context.fillRect(120,160,20,20);
-// toit maison
-context.globalCompositeOperation = "destination-over";
-context.fillStyle = 'peru';
-context.fillRect(50,100,100,100);
-context.translate( 100, 0 );
-context.rotate(45 * Math.PI / 180);
-context.fillStyle = 'peru';
-context.fillRect(50,100,100,100);
-// test
-// ctx.save();
-// ctx.rotate( rotationAmountInRadians );
-// ctx.translate( -objectRotationCenterX, -objectRotationCenterY );
-// ctx.drawImage( myImageOrCanvas, 0, 0 );
-// ctx.restore();
+
+context.fillStyle = '#00A550';
+context.fillRect(100,50,100,25);
+context.fillRect(75,75,150,25);
+
+
+context.beginPath();
+context.fillStyle = '#CECECE';
+context.lineWidth="1";
+context.arc(87, 120, 20, 0, 2 * Math.PI);
+context.fill();
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.fillStyle = '#CECECE';
+context.lineWidth="1";
+context.arc(212, 120, 20, 0, 2 * Math.PI);
+context.fill();
+context.stroke();
+context.closePath();
