f82e53ec7322f00abef019962b44224ecae08c76
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-18 16:04:26 +0200

    debut

--- exercice 1/assets/js/script.js
+++ exercice 1/assets/js/script.js
@@ -1,16 +1,2 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
-
-context.beginPath();
-context.moveTo(50, 100);
-context.lineTo(90,100);
-context.lineTo(70,150);
-context.closePath();
-context.fillStyle = '#AA6522';
-context.fill();
-
-context.beginPath();
-context.moveTo(50, 100);
-context.quadraticCurveTo(75, 50, 90,100);
-context.fillStyle ='#8A0908';
-context.fill();
